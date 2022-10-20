21db6e44d2eea1469db21719fc799f355090b908
Carolina Jimenez Moreno <cjimenezm0794@gmail.com>
2022-10-19 23:24:14 -0500

    (#4) Add register page

--- lib/pages/register_page.dart
+++ lib/pages/register_page.dart
@@ -8,8 +8,62 @@ class RegisterPage extends StatefulWidget {
 }
 
 class _RegisterPageState extends State<RegisterPage> {
+  final _name = TextEditingController();
+  final _email = TextEditingController();
+  final _password = TextEditingController();
+  final _repPassword = TextEditingController();
+
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+        body: Padding(
+          padding: const EdgeInsets.symmetric(horizontal:8, vertical:16),
+          child: Center (
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: <Widget>[
+                  const Image(image:AssetImage('assets/images/logo.png')),
+                  const SizedBox(
+                    height: 16.0,
+                  ),
+                  TextFormField(
+                    controller: _name,
+                    decoration: const InputDecoration(
+                        border:  OutlineInputBorder(), labelText: 'Nombre'),
+                    keyboardType: TextInputType.text,
+                  ),
+                  const SizedBox(
+                    height: 16.0,
+                  ),
+                  TextFormField(
+                    controller: _email,
+                    decoration: const InputDecoration(
+                        border:  OutlineInputBorder(), labelText: 'Correo'),
+                    keyboardType: TextInputType.text,
+                  ),
+                  const SizedBox(
+                    height: 16.0,
+                  ),
+                  TextFormField(
+                    controller: _password,
+                    decoration: const InputDecoration(
+                        border:  OutlineInputBorder(), labelText: 'Contraseña'),
+                    keyboardType: TextInputType.text,
+                  ),
+                  const SizedBox(
+                    height: 16.0,
+                  ),
+                  TextFormField(
+                    controller: _repPassword,
+                    decoration: const InputDecoration(
+                        border:  OutlineInputBorder(), labelText: 'Confirmar Contraseña'),
+                    keyboardType: TextInputType.text,
+                  ),
+                ],
+              )
+
+          ),
+        )
+    );
   }
 }
